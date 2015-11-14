a10dbe5cc6e1e680f8d689fe8bdcf54c5b06b8b0
Nicholas Bellinger <nab@linux-iscsi.org>
2015-11-14 14:06:18 -0800

    vhost/nvme: Populate vhost_nvme_hwi_work with 0x14 CC + friends
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- drivers/target/vhost_nvme/vhost_nvme_hwi.c
+++ drivers/target/vhost_nvme/vhost_nvme_hwi.c
@@ -1,9 +1,15 @@
 /*
- * In-kernel vhost-nvme queue handling
+ * In-kernel vhost-nvme hardware-interface emulation
  *
  * Copyright (c) 2015 Datera, Inc
- *  
- * Functions for hardware-interface emulation handling
+ *
+ * Parts based on QEMU NVMe-HI emulation code:
+ *
+ * Copyright (c) 2012, Intel Corporation
+ *
+ * Written by Keith Busch <keith.busch@intel.com>
+ *
+ * This code is licensed under the GNU GPL v2 or later.
  */
 
 #include <linux/init.h>
@@ -16,10 +22,71 @@
 
 #include "vhost_nvme_base.h"
 
+static int
+vhost_nvme_hwi_map_sgl(struct vhost_nvme_hba *hba, struct vhost_nvme_cmd *cmd)
+{
+	return -ENOSYS;
+}
+
 static void
 vhost_nvme_hwi_work(struct work_struct *work)
 {
-	return;
+	struct vhost_nvme_cmd *cmd = container_of(work,
+				struct vhost_nvme_cmd, work);
+	u8 addr = 0xff; // XXX: FIXME
+	uint64_t data = 0xffffffff; // XXX: FIXME
+	int rc;
+
+	rc = vhost_nvme_hwi_map_sgl(NULL, cmd);
+	if (rc) {
+		// XXX: handle frame failure back to userspace
+		BUG_ON();
+	}
+
+	switch (addr) {
+	case 0x14: // Used for initial create config controller queue
+		/*
+		 * Windows first sends data, then sends enable bit
+		 * for initial configuration controller MMIO WRITE
+		 *
+		 * XXX: Need to check existing CC state..?
+		 */
+		if (!NVME_CC_EN(data) && !NVME_CC_EN(n->bar.cc) &&
+		    !NVME_CC_SHN(data) && !NVME_CC_SHN(n->bar.cc)) {
+			n->bar.cc = data;
+		}
+		/*
+		 * Check NVME_CC_EN state to determine if configuration
+		 * controller needs initial NVMe-HI setup, or explicit
+		 * cleanup for vhost PCIe MSI-X device.
+		 */
+		if (NVME_CC_EN(data) && !NVME_CC_EN(n->bar.cc)) {
+			// vhost_nvme_start_cc();
+		} else if (!NVME_CC_EN(data) && NVME_CC_EN(n->bar.cc)) {
+			// vhost_nvme_clear_cc();
+		}
+		if (NVME_CC_SHN(data) && !(NVME_CC_SHN(n->bar.cc))) {
+			// vhost_nvme_clear_cc();
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+static u64 vhost_nvme_get_hwi_context(u8 __user *frame_addr)
+{
+	u64 context;
+	off_t context_offset = 0; // XXX FIXME
+	int ret;
+
+	ret = copy_from_user(&context, frame_addr + context_offset, sizeof(u64));
+	if (unlikely(ret)) {
+		pr_warn("Unable to get context from frame addr %p, ret %d\n",
+			frame_attr, ret);
+		context = (u64)-1;
+	}
+	return context;
 }
 
 static struct vhost_nvme_cmd *
